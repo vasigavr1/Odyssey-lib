f5a57f568ff5ae2f859ffbd144e6f880aae7afaf
vasigavr1 <vasigavr1@gmail.com>
2020-09-15 19:11:18 +0100

    CHT one leader & multiple leaders

--- include/general_util/top.h
+++ include/general_util/top.h
@@ -74,9 +74,9 @@ typedef struct key mica_key_t;
 #define MACHINE_NUM 5
 
 
-#define WORKERS_PER_MACHINE 20
-#define SESSIONS_PER_THREAD 20
-#define WRITE_RATIO 500 //Warning write ratio is given out of a 1000, e.g 10 means 10/1000 i.e. 1%
+#define WORKERS_PER_MACHINE 35
+#define SESSIONS_PER_THREAD 64
+#define WRITE_RATIO 1000 //Warning write ratio is given out of a 1000, e.g 10 means 10/1000 i.e. 1%
 #define RMW_RATIO 10// this is out of 1000, e.g. 10 means 1%
 #define ENABLE_MULTICAST_ 0
 
